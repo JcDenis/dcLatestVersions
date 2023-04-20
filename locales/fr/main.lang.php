c4cbc38983d97beb78681cf09259afe652bf6738
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-20 23:10:26 +0200

    update namespace

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -9,9 +9,11 @@
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Download Dotclear %s']                                                   = 'Télécharger Dotclear %s';
-l10n::$locales['Dotclear\'s latest versions']                                            = 'Dernières versions de Dotclear';
-l10n::$locales['Show Dotclear\'s latest versions on dashboards.']                        = 'Afficher les dernières versions de Dotclear sur le tableau de bord';
-l10n::$locales['Show the latest available versions of Dotclear']                         = 'Afficher les dernières versions disponibles de Dotclear';
-l10n::$locales['Text (%r = release, %v = version, %u = url):']                           = 'Texte (%r = type, %v = version, %u = url) :';
-l10n::$locales['<strong>%r: </strong> <a href="%u" title="Download Dotclear %v">%v</a>'] = '<strong>%r : </strong> <a href="%u" title="Télécharger Dotclear %v">%v</a>';
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Download Dotclear %s']                                                   = 'Télécharger Dotclear %s';
+L10n::$locales['Dotclear\'s latest versions']                                            = 'Dernières versions de Dotclear';
+L10n::$locales['Show Dotclear\'s latest versions on dashboards.']                        = 'Afficher les dernières versions de Dotclear sur le tableau de bord';
+L10n::$locales['Show the latest available versions of Dotclear']                         = 'Afficher les dernières versions disponibles de Dotclear';
+L10n::$locales['Text (%r = release, %v = version, %u = url):']                           = 'Texte (%r = type, %v = version, %u = url) :';
+L10n::$locales['<strong>%r: </strong> <a href="%u" title="Download Dotclear %v">%v</a>'] = '<strong>%r : </strong> <a href="%u" title="Télécharger Dotclear %v">%v</a>';
